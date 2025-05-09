dccc26bde4c1a81e68be1988ef2f44cd99a878d2
riteesh-naidu <riteesh56@gmail.com>
2025-05-09 11:53:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -22,6 +23,7 @@ def services():
 def contact():
     return render_template('contact.html')
 
+# Main block for deployment (bind to 0.0.0.0 and use dynamic port)
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
